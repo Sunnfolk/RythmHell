22c51435e79fa1d5859491df6b4b7b80cbdec4c3
Resonid <erlend.kaarigstad@lyse.net>
2021-10-28 17:34:29 +0200

    noe light script stuff

--- Assets/Scripts/Menu&World/LightScript.cs
+++ Assets/Scripts/Menu&World/LightScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,37 +9,39 @@ namespace Menu_World
     public class LightScript : MonoBehaviour
     {
         private Light2D m_FailLight;
+        public float speed= 0.01f;
 
         private void Start()
         {
             m_FailLight = GetComponent<Light2D>();
         }
 
-        public void FadeLights()
-        {
-            StartCoroutine(nameof(Timer));
-        }
-
         private void Update()
         {
-            if (Keyboard.current.gKey.wasPressedThisFrame)
+            if (Keyboard.current.eKey.wasPressedThisFrame)
             {
                 FadeLights();
             }
         }
+
+        public void FadeLights()
+        {
+            StartCoroutine(nameof(Timer));
+        }
+        
 
         private IEnumerator Timer()
         {
             for (int i = 0; i <= 10; i++)
             {
                 m_FailLight.intensity = i / 10f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(speed);
             }
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(speed);
             for (int i = 10; i >= 0; i--)
             {
                 m_FailLight.intensity = i / 10f;
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(speed);
             }
         }
     }
