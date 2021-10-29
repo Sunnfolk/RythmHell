8b115d8083ffce2a04f858dda93a7a285cbe4706
Resonid <erlend.kaarigstad@lyse.net>
2021-10-29 19:12:51 +0200

    Background scroll

--- Assets/BackgroundMovement.cs
+++ Assets/BackgroundMovement.cs
@@ -1,14 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    private bool m_GoingUp;
 
     private void Update()
     {
-        
+        if (transform.position.y >= -14.7f && !m_GoingUp)
+        {
+            transform.Translate(Vector2.down * movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_GoingUp = true;
+        }
+
+        if (transform.position.y <= 14.7f && m_GoingUp)
+        {
+            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_GoingUp = false;
+        }
+
     }
 }
